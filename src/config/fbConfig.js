e6a46be043630699a0b70118f59fb45782700f6b
Sidharth Shamshabad <sid@techvoe.com>
2020-07-01 08:50:23 -0400

    finished setting up actions with firebase config

---
+++ src/config/fbConfig.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCctKDj2OgyhwBDm8y5DvjYxvz0E6HLwT8",
+    authDomain: "mario-plan-1a6f8.firebaseapp.com",
+    databaseURL: "https://mario-plan-1a6f8.firebaseio.com",
+    projectId: "mario-plan-1a6f8",
+    storageBucket: "mario-plan-1a6f8.appspot.com",
+    messagingSenderId: "491905953126",
+    appId: "1:491905953126:web:d4568dba06cc7e8628aca7",
+    measurementId: "G-0EP285ZV3Q"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+firebase.analytics();
+
+export default firebase;
