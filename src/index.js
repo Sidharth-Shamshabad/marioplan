659c86a12a025ae58bcc9128af864ec2ed569419
Sidharth Shamshabad <sid@techvoe.com>
2020-07-01 10:55:26 -0400

    pulls data from firebase and displays it on dashboard

--- src/index.js
+++ src/index.js
@@ -4,23 +4,43 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './store/reducers/rootReducer';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { reduxFirestore, getFirestore } from 'redux-firestore';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
-import fbConfig from './config/fbConfig';
-import firebase from "firebase/app";
-//links firebase and firestore with redux
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-    reduxFirestore(firebase, fbConfig) // redux bindings for firestore
-  )
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+
+import {createFirestoreInstance, reduxFirestore, getFirestore } from 'redux-firestore'
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
+import firebase from 'firebase/app'
+import fbConfig from './config/fbConfig'
+
+
+const store = createStore(rootReducer,
+    compose(
+        applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+        reduxFirestore(fbConfig)
+    )
 );
 
-export default store;
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+const rrfConfig = {
+    userProfile: 'users',
+  }
+const rrfProps = {
+    firebase,
+    config: rrfConfig,
+    dispatch: store.dispatch,
+    createFirestoreInstance,
+
+
+}
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+            <App />
+        </ReactReduxFirebaseProvider>
+    </Provider>,
+    document.getElementById('root'));
 serviceWorker.unregister();
