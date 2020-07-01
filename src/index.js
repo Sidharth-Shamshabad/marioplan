1ebd151703f98f44780b6cafcf16704fae020ccf
Sidharth Shamshabad <sid@techvoe.com>
2020-07-01 15:00:19 -0400

    app now checks aunthentication before rendering

--- src/index.js
+++ src/index.js
@@ -14,6 +14,9 @@ import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
 import firebase from 'firebase/app'
 import fbConfig from './config/fbConfig'
 
+import { useSelector  } from 'react-redux'
+  import { isLoaded  } from 'react-redux-firebase';
+
 
 const store = createStore(rootReducer,
     compose(
@@ -23,24 +26,37 @@ const store = createStore(rootReducer,
 );
 
 
+
 const rrfConfig = {
     userProfile: 'users',
   }
-const rrfProps = {
-    firebase,
-    config: rrfConfig,
-    dispatch: store.dispatch,
-    createFirestoreInstance,
-
-
-}
-
-
-ReactDOM.render(
-    <Provider store={store}>
-        <ReactReduxFirebaseProvider {...rrfProps}>
-            <App />
-        </ReactReduxFirebaseProvider>
-    </Provider>,
-    document.getElementById('root'));
+  
+  
+  
+  
+  const rrfProps = {
+      firebase,
+      config: fbConfig,
+      dispatch: store.dispatch,
+      createFirestoreInstance,
+      userProfile: 'users', // where profiles are stored in database
+      presence: 'presence', // where list of online users is stored in database
+      sessions: 'sessions'
+  }
+  
+  
+  
+  
+  function AuthIsLoaded({ children }) {
+      const auth = useSelector(state => state.firebase.auth)
+      if (!isLoaded(auth)) return <div>Loading Screen...</div>;
+          return children
+  }
+  
+  
+  
+  
+ ReactDOM.render(<Provider store={store}> <ReactReduxFirebaseProvider {...rrfProps}> <AuthIsLoaded><App /> </AuthIsLoaded></ReactReduxFirebaseProvider></Provider>, document.getElementById('root'));
+  
+  
 serviceWorker.unregister();
