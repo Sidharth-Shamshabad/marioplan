cc055050f63bd38004d50929534cf2f818ee7ae9
Sidharth Shamshabad <sid@techvoe.com>
2020-07-01 10:11:16 -0400

    create project page pushes title and content to database in the projects collection

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,17 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, {getFirestore}) => {
         // make async call to database
-        dispatch({ type: 'CREATE_PROJECT', project: project })
+        const database = getFirestore();
+        database.collection('projects').add({
+            ...project, //spread operator. spreads the object and neatly pushes it as desired category collections with the information already added. whatever is added after is added to the object
+            authorFirstName: 'Sidharth',
+            authorLastName: 'Shamshabad',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => { //once object is pushed into database then do whatever is inside .then func.
+            dispatch({ type: 'CREATE_PROJECT', project: project });
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err});
+        })
     }
 }
