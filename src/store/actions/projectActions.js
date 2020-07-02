0f4a67f0321afd72c92adf3be9df1800f8665461
Sidharth Shamshabad <sid@techvoe.com>
2020-07-02 09:50:36 -0400

    updates project details/creating projects to firebase and render correct info

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,11 +2,13 @@ export const createProject = (project) => {
     return (dispatch, getState, {getFirestore}) => {
         // make async call to database
         const database = getFirestore();
+        const profile = getState().firebase.profile;
+        const authorId = getState().firebase.auth.uid
         database.collection('projects').add({
             ...project, //spread operator. spreads the object and neatly pushes it as desired category collections with the information already added. whatever is added after is added to the object
-            authorFirstName: 'Sidharth',
-            authorLastName: 'Shamshabad',
-            authorId: 12345,
+            authorFirstName: profile.firstName,
+            authorLastName: profile.lastName,
+            authorId: authorId,
             createdAt: new Date()
         }).then(() => { //once object is pushed into database then do whatever is inside .then func.
             dispatch({ type: 'CREATE_PROJECT', project: project });
